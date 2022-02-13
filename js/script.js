9d804e5bd462e0c9f84c36ec04007fcbc2e17119
my first git <asadpatwary951@gmail.com>
2022-02-13 18:53:57 +0600

    Gym finshed

--- js/script.js
+++ js/script.js
@@ -15,6 +15,30 @@ $(function () {
     selector: '.my-video-links',
   });
 
+  // slick slider for testimonial part
+  $('.test_slider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: true,
+    arrows:false,
+  });
+  // counter up js
+  $('.count_up').counterUp();
+
+  // slick slider for logo slider
+
+  $('.logo_all').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows:true,
+    prevArrow:'<i class="fas fa-chevron-left prev_arrow"></i>',
+    nextArrow:'<i class="fas fa-chevron-right next_arrow"></i>',
+  });
+
 
 
 
